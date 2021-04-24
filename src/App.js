53921142077df9b5e5324ae061e62ccd85f04968
Lucas Alonso <lucasalonso11800@gmail.com>
2021-04-24 16:28:08 -0300

    Created basic GlobalDashboard

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 
 import Navbar from './components/Navbar';
+import GlobalDashboard from './components/GlobalDashboard';
 
 function App() {
-  return (
-    <div className="background">
-        <Navbar />
-    </div>
-  );
+    return (
+        <div className="background">
+            <Navbar />
+            <GlobalDashboard />
+        </div>
+    );
 }
 
 export default App;
