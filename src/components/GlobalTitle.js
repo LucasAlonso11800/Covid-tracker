8a9aad55dcd0a1ab4aad318aee2c50304a508523
Lucas Alonso <lucasalonso11800@gmail.com>
2021-04-29 15:33:31 -0300

    Deleted unused import

--- src/components/GlobalTitle.js
+++ src/components/GlobalTitle.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Card, CardContent, Typography, CircularProgress } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 
 const useStyles = makeStyles(() => ({
     root: {
