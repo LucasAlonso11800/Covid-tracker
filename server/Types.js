8b1d06817db64d826adc93f94259c987a2e9a368
Lucas <lucasalonso11800@gmail.com>
2021-07-17 20:27:17 -0300

    Get increase data

--- server/Types.js
+++ server/Types.js
@@ -1,16 +1,17 @@
 const {
     GraphQLObjectType,
     GraphQLList,
-    GraphQLInt
+    GraphQLInt,
+    GraphQLFloat
 } = require('graphql');
 
 const GlobalDataType = new GraphQLObjectType({
     name: 'GlobalData',
     fields: () => ({
         today_confirmed: { type: GraphQLInt },
-        today_deaths: { type: GraphQLInt },
-        today_open_cases: { type: GraphQLInt },
         today_recovered: { type: GraphQLInt },
+        today_open_cases: { type: GraphQLInt },
+        today_deaths: { type: GraphQLInt }
     })
 });
 
@@ -18,8 +19,8 @@ const CountryDailyInfoType = new GraphQLObjectType({
     name: 'CountryDailyInfo',
     fields: () => ({
         new_confirmed: { type: new GraphQLList(GraphQLInt) },
-        new_deaths: { type: new GraphQLList(GraphQLInt) },
         new_recovered: { type: new GraphQLList(GraphQLInt) },
+        new_deaths: { type: new GraphQLList(GraphQLInt) }
     })
 });
 
@@ -27,10 +28,20 @@ const CountryTotalsType = new GraphQLObjectType({
     name: 'CountryTotals',
     fields: () => ({
         total_confirmed: { type: new GraphQLList(GraphQLInt) },
-        total_deaths: { type: new GraphQLList(GraphQLInt) },
         total_recovered: { type: new GraphQLList(GraphQLInt) },
         total_open_cases: { type: new GraphQLList(GraphQLInt) },
+        total_deaths: { type: new GraphQLList(GraphQLInt) },
     })
 });
 
-module.exports = { GlobalDataType, CountryDailyInfoType, CountryTotalsType };
+const CountryDailyIncreaseType = new GraphQLObjectType({
+    name: 'CountryDailyIncrease',
+    fields: () => ({
+        confirmed_increase: { type: GraphQLFloat },
+        recovered_increase: { type: GraphQLFloat },
+        open_cases_increase: { type: GraphQLFloat },
+        deaths_increase: { type: GraphQLFloat },
+    })
+});
+
+module.exports = { GlobalDataType, CountryDailyInfoType, CountryTotalsType, CountryDailyIncreaseType };
